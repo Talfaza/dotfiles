97563bab0c1e237ccde7ffdca0ef47a9495d5982
Talfaza <42444571+Talfaza@users.noreply.github.com>
2024-01-12 14:29:35 +0100

    modules

---
+++ HP 13dw/nvim/plugins/moonfly.lua
@@ -0,0 +1,14 @@
+return 
+{
+		"bluz71/vim-moonfly-colors",
+		name = "moonfly", 
+		lazy = false, priority = 1000, 
+		config = function()
+						vim.cmd.colorscheme "moonfly" 
+				end
+}
+
+
+
+
+
---
+++ HP 13dw/nvim/plugins/telescope.lua
@@ -0,0 +1,14 @@
+return 	{
+		'nvim-telescope/telescope.nvim', 
+		tag = '0.1.5',
+		dependencies = { 'nvim-lua/plenary.nvim' },
+		config = function()
+				 local builtin = require('telescope.builtin')
+		vim.keymap.set('n','<C-p>',builtin.find_files,{})
+		vim.keymap.set('n', '<leader>s', builtin.live_grep, {})
+
+
+
+				 end
+}
+
