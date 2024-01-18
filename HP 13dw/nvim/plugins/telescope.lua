7b641338db8ae8fc113283415f97d519c02e6b87
Talfaza <42444571+Talfaza@users.noreply.github.com>
2024-01-18 17:11:24 +0100

    telescope.lua new module

--- HP 13dw/nvim/plugins/telescope.lua
+++ HP 13dw/nvim/plugins/telescope.lua
@@ -1,4 +1,8 @@
-return 	{
+return {
+
+
+
+					{
 		'nvim-telescope/telescope.nvim', 
 		tag = '0.1.5',
 		dependencies = { 'nvim-lua/plenary.nvim' },
@@ -10,5 +14,27 @@ return 	{
 
 
 				 end
-}
+},
 
+
+
+ {
+
+
+
+		'nvim-telescope/telescope-ui-select.nvim',
+		config = function()
+				require("telescope").setup {
+				  extensions = {
+					["ui-select"] = {
+					  require("telescope.themes").get_dropdown {
+					  }
+
+					}
+				  }
+				}
+				require("telescope").load_extension("ui-select")
+		end
+
+}
+}
