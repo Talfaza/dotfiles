fc4488e60aa660c3fc73cf54e43a866b775d2ab4
Talfaza <42444571+Talfaza@users.noreply.github.com>
2024-01-18 17:12:08 +0100

    alpha.lua

---
+++ HP 13dw/nvim/plugins/alpha.lua
@@ -0,0 +1,37 @@
+return {
+
+		 "goolord/alpha-nvim",
+		 dependencies = { 'nvim-tree/nvim-web-devicons' },
+
+     config = function ()
+         local alpha = require'alpha'
+         local dashboard = require'alpha.themes.dashboard'
+		 dashboard.section.header.val = {
+
+
+
+								[[⡏⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿]],
+								[[⣿⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠉⠁⠀⣿]],
+								[[⣿⣧⡀⠀⠀⠀⠀⠙⠿⠿⠿⠻⠿⠿⠟⠿⠛⠉⠀⠀⠀⠀⠀⣸⣿]],
+								[[⣿⣿⣷⣄⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿]],
+								[[⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣴⣿⣿⣿⣿]],
+								[[⣿⣿⣿⡟⠀⠀⢰⣹⡆⠀⠀⠀⠀⠀⠀⣭⣷⠀⠀⠀⠸⣿⣿⣿⣿]],
+								[[⣿⣿⣿⠃⠀⠀⠈⠉⠀⠀⠤⠄⠀⠀⠀⠉⠁⠀⠀⠀⠀⢿⣿⣿⣿]],
+								[[⣿⣿⣿⢾⣿⣷⠀⠀⠀⠀⡠⠤⢄⠀⠀⠀⠠⣿⣿⣷⠀⢸⣿⣿⣿]],
+								[[⣿⣿⣿⡀⠉⠀⠀⠀⠀⠀⢄⠀⢀⠀⠀⠀⠀⠉⠉⠁⠀⠀⣿⣿⣿]],
+								[[⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿]],
+								[[⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿]],
+
+
+
+
+
+
+	 }
+
+
+
+
+         alpha.setup(dashboard.config)
+     end
+ }
