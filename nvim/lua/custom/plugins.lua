37b630a65b24f3a7359c707d5446d3607fadaa62
Talfaza <charifyahia4@gmail.com>
2024-02-22 17:41:25 +0100

    add: c++ lsp (clangd)

---
+++ nvim/lua/custom/plugins.lua
@@ -0,0 +1,20 @@
+local plugins = {
+{
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "clangd",
+        "clang-format",
+        "codelldb",
+      }
+    }
+  }
+}
+return plugins
