1fde4fd233dc1040f6697db7a0fb231779806024
Talfaza <charifyahia4@gmail.com>
2024-09-30 08:57:57 +0100

    fix: typo fix

--- sync.sh
+++ sync.sh
@@ -2,7 +2,7 @@
 echo "Syncing files to git repo"
 rsync -av --progress ~/.config/i3/ ~/gitthings/dotfiles/i3/
 rsync -av --progress  ~/gitthings/dotfiles/walls/ ~/.config/i3/walls
-qrsync -av --progress ~/.config/picom/ ~/gitthings/dotfiles/picom/
+rsync -av --progress ~/.config/picom/ ~/gitthings/dotfiles/config/picom/
 
 if [[ $? -eq 0 ]]; then 
 	echo "Files synced ! "
